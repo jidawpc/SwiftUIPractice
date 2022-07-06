ac0b7b25646178d41becefbd89aee42b4687b262
pengcheng.wang <pengcheng.wang@zkh360.com>
2022-07-06 16:20:58 +0800

    first commit

---
+++ SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by 汪鹏程 on 2022/7/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
